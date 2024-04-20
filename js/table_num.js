6dae904b0cfc2a3bcd5db147b958c27039106bce
wdingsoft <wdingbox@gmail.com>
2024-04-20 13:37:55 -0400

    js

---
+++ js/table_num.js
@@ -0,0 +1,15 @@
+
+function table_num() {
+    var shref = window.location.href
+    var idx = 1 + shref.lastIndexOf("#")
+    console.log(idx)
+    var num = shref.substring(idx)
+    console.log(num)
+    var etable = document.getElementsByTagName("table")[0];
+
+    let b = document.createElement("b");
+    b.innerHTML = `Table ${num}`
+    etable.prepend(b)
+    console.log("Table " + etable)
+
+}
